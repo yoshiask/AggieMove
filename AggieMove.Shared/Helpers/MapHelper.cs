5df90d3a83a9d3a9e15f8d95fb45338b1e6b07f1
Yoshi Askharoun <jjask7@gmail.com>
2022-03-14 15:32:40 -0500

    Removed some unused helpers from MapHelper

--- AggieMove.Shared/Helpers/MapHelper.cs
+++ AggieMove.Shared/Helpers/MapHelper.cs
@@ -20,42 +20,6 @@ namespace AggieMove.Helpers
         public static readonly MapPoint TAMU_CENTER_POINT = new MapPoint(30.610190, -96.344800);
         public static readonly SpatialReference BUS_ROUTES_SR = new SpatialReference(3857);
 
-        public static async Task<RouteResult> LoadRouter(MapView mapView)
-        {
-            var routeSourceUri = new Uri("https://gis.tamu.edu/arcgis/rest/services/Routing/20210825/NAServer/Route");
-            var routeTask = await RouteTask.CreateAsync(routeSourceUri);
-
-            // get the default route parameters
-            var routeParams = await routeTask.CreateDefaultParametersAsync();
-            // explicitly set values for some params
-            routeParams.ReturnDirections = true;
-            routeParams.ReturnRoutes = true;
-            if (mapView.SpatialReference != null)
-                routeParams.OutputSpatialReference = mapView.SpatialReference;
-
-            // create a Stop for my location
-            var curLoc = await SpatialHelper.GetCurrentLocation();
-            var myLocation = new MapPoint(curLoc.Value.Longitude, curLoc.Value.Latitude, SpatialReferences.Wgs84);
-            var stop1 = new Stop(myLocation);
-
-            // create a Stop for your location
-            var yourLocation = new MapPoint(-96.34131, 30.61247, SpatialReferences.Wgs84);
-            var stop2 = new Stop(yourLocation);
-
-            // assign the stops to the route parameters
-            var stopPoints = new List<Stop> { stop1, stop2 };
-            routeParams.SetStops(stopPoints);
-
-            try
-            {
-                return await routeTask.SolveRouteAsync(routeParams);
-            }
-            catch
-            {
-                return null;
-            }
-        }
-
         public static void LoadMap(double lat, double lon, MapView mapView)
         {
             mapView.Map = new Map(
@@ -75,27 +39,6 @@ namespace AggieMove.Helpers
         public static void LoadMap(MapView mapView)
         {
             LoadMap(TAMU_CENTER_POINT.X, TAMU_CENTER_POINT.Y, mapView);
-        }
-
-        public static async Task SetViewpointToCurrentLocation(MapView mapView, GraphicsOverlay mapGraphics,
-            Windows.Foundation.TypedEventHandler<Windows.Devices.Geolocation.Geolocator, Windows.Devices.Geolocation.PositionChangedEventArgs> PositionChangedHandler,
-            bool zoomToLocation = true, int scale = 2000)
-        {
-            var currentLoc = await SpatialHelper.GetCurrentLocation();
-            if (currentLoc.HasValue)
-            {
-                var currentLocPoint = CreateRouteStop(currentLoc.Value.Latitude, currentLoc.Value.Longitude, System.Drawing.Color.Red);
-                currentLocPoint.Attributes.Add("id", "currentLocation");
-                mapGraphics.Graphics.Add(currentLocPoint);
-                if (zoomToLocation)
-                {
-                    await mapView.SetViewpointCenterAsync(currentLoc.Value.Latitude, currentLoc.Value.Longitude);
-                    await mapView.SetViewpointScaleAsync(scale);
-                }
-
-                if (PositionChangedHandler != null)
-                    SpatialHelper.Geolocator.PositionChanged += PositionChangedHandler;
-            }
         }
 
         public static Graphic CreateRouteStop(double lat, double lon, Color fill)
